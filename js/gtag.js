9f22b2192ad88ac7ecd597e2d96971e995219538
FormPrint <overprint.tw@gmail.com>
2020-11-26 17:20:02 +0800

    add TW00003_V1 form set

--- js/gtag.js
+++ js/gtag.js
@@ -1,9 +1,14 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LRGSVQSH1Y"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-LRGSVQSH1Y');
-</script>
+{
+createTime: {
+nanos: 259000000,
+seconds: "1606008639",
+},
+defaultUri: "https://www.formprint.dev",
+displayName: "電腦套印專家",
+measurementId: "LRGSVQSH1Y",
+name: "properties/253795439/webDataStreams/2172346564",
+updateTime: {
+nanos: 259000000,
+seconds: "1606008639",
+},
+},
