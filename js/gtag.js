be32debf9d316849e5272c1a2cddd054fe51e4f3
FormPrint <overprint.tw@gmail.com>
2020-12-03 20:14:21 +0800

    update gtag.js

--- js/gtag.js
+++ js/gtag.js
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z1QFBHDH3G"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VR1Y3YGNYP"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-Z1QFBHDH3G');
+  gtag('config', 'G-VR1Y3YGNYP');
 </script>
